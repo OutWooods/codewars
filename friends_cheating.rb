d72beb89fe8ea2be16465be7c6027a0b4c9ffc7f
Tom Grand <thomas.a.grand@gmail.com>
2017-12-20 14:16:21 +0000

    New solution to problem

--- friends_cheating.rb
+++ friends_cheating.rb
@@ -3,21 +3,20 @@ def removNb(n)
   number_list = []
   sum = (1..n).inject(0, &:+)
   puts sum
-  low_limit = sum / n
-  puts low_limit
-  upper_limit = Math.sqrt(sum).to_i
-   a = upper_limit
-   b = upper_limit
+  low_limit = sum / n  
+  mid_point = Math.sqrt(sum).to_i
+   a = mid_point
+   b = mid_point
     until (a == low_limit || b > n) 
       total = sum - (a + b)
-       product = a * b
+      product = a * b
        if total == product 
          number_list << [a, b]
          a -= 1
       elsif total < product 
          a -= 1
       else 
-          b += 1
+         b += 1
       end    
     end
 
