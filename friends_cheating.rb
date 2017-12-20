1d53229aef228ffd739f355a9751f23fbbacc4bb
Tom Grand <thomas.a.grand@gmail.com>
2017-12-20 14:10:15 +0000

    Update friends_cheating.rb

--- friends_cheating.rb
+++ friends_cheating.rb
@@ -2,16 +2,26 @@
 def removNb(n)
   number_list = []
   sum = (1..n).inject(0, &:+)
-
-  lower_limit = sum / n   #I think this is the lowest number that will work
-  mid_way = Math.root(n)   #This is midway between the lowest and the top number
-
-  (lower_limit..mid_way+1).each do |numA|
-    n.downto(mid_way-1) do |numB|
-      number_list << [numA, numB] if sum - (numA + numB) == numA * numB
+  puts sum
+  low_limit = sum / n
+  puts low_limit
+  upper_limit = Math.sqrt(sum).to_i
+   a = upper_limit
+   b = upper_limit
+    until (a == low_limit || b > n) 
+      total = sum - (a + b)
+       product = a * b
+       if total == product 
+         number_list << [a, b]
+         a -= 1
+      elsif total < product 
+         a -= 1
+      else 
+          b += 1
+      end    
     end
-  end
-  number_list
-  reversed = number_list.map {|arr| arr.reverse}
-  number_list + reversed
+
+  other_answers = number_list.map{|arr| arr.reverse}
+  answer = number_list + other_answers
+  answer.sort!
 end
