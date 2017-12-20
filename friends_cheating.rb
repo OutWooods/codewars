a3c84cbbc052c3332d78e026526a99c4fea45938
Tom Grand <thomas.a.grand@gmail.com>
2017-12-20 13:43:20 +0000

    Initialise repo

---
+++ friends_cheating.rb
@@ -0,0 +1,15 @@
+
+def removNb(n)
+  number_list = []
+  sum = (1..n).inject(0, &:+)
+
+  lower_limit = sum / n   #I think this is the lowest number that will work
+  mid_way = (sum + n) / 2   #This is midway between the lowest and the top number
+
+  (lower_limit..mid_way+1).each do |numA|
+    n.downto(mid_way-1) do |numB|
+      number_list << [numA, numB] if sum - (numA + numB) == numA * numB
+    end
+  end
+  number_list
+end
