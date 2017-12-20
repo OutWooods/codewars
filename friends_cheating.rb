55b1f2109a8e74aa581c31db15b06f41fc1e2691
Tom Grand <thomas.a.grand@gmail.com>
2017-12-20 14:20:14 +0000

    Refactor solution

--- friends_cheating.rb
+++ friends_cheating.rb
@@ -2,20 +2,22 @@
 def removNb(n)
   number_list = []
   sum = (1..n).inject(0, &:+)
-  puts sum
+ 
+  
   low_limit = sum / n  
   mid_point = Math.sqrt(sum).to_i
-   a = mid_point
-   b = mid_point
+  
+   a, b = mid_point, mid_point
+  
     until (a == low_limit || b > n) 
-      total = sum - (a + b)
-      product = a * b
+       total = sum - (a + b)
+       product = a * b
        if total == product 
-         number_list << [a, b]
-         a -= 1
-      elsif total < product 
-         a -= 1
-      else 
+           number_list << [a, b]
+           a -= 1
+       elsif total < product 
+           a -= 1
+        else 
          b += 1
       end    
     end
