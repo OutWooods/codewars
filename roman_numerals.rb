db9b8ebbad0eecf892c83e44d9c675a69b730e1e
Tom Grand <thomas.a.grand@gmail.com>
2017-12-22 17:58:18 +0000

    Replace with recursive solution
    This is hopefully more easy to maintain

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,22 +1,22 @@
 
 
-def solution(roman)
- numerals = {"I" => 1, "IV" => 4,"V" => 5, "IX" => 9, "X" => 10,
+def solution(roman, total=0) 
+	puts total
+numerals = {"I" => 1, "IV" => 4,"V" => 5, "IX" => 9, "X" => 10,
              "XL" => 40, "L" => 50,"XC" => 90, "C" => 100, "D" => 500, 
               "CM" => 900, "M" => 1000}
- total = 0             
-   until roman == ""
-      if numerals[roman[0..1]].nil? || roman[1].nil?
-      	puts "Yes"
+
+ return total if roman.length == 0
+ return total += numerals[roman] if roman.length == 1
+
+   if numerals[roman[0..1]].nil? 
         total += numerals[roman[0]]
        	roman.slice!(0,1)
-       else 
-       	puts "no"
+   else 
         total += numerals[roman[0..1]]
       	roman.slice!(0, 2)
-       end
    end   
-  total 
+  total = solution(roman, total)
 end
 
 puts solution("MDCLXVI")
