2e5b9f4047f69926258b2da015f27bb9310a772f
Tom Grand <thomas.a.grand@gmail.com>
2017-12-22 17:49:00 +0000

    Add first working solution
    Note: its all in one method, I want to make sure each method
    has a single responsibility.

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,8 +1,22 @@
+
+
 def solution(roman)
  numerals = {"I" => 1, "IV" => 4,"V" => 5, "IX" => 9, "X" => 10,
-             "XL" => 40, "L" => 50,"XC" => 90, "C" => 100, "D" => 100, 
+             "XL" => 40, "L" => 50,"XC" => 90, "C" => 100, "D" => 500, 
               "CM" => 900, "M" => 1000}
- 
+ total = 0             
+   until roman == ""
+      if numerals[roman[0..1]].nil? || roman[1].nil?
+      	puts "Yes"
+        total += numerals[roman[0]]
+       	roman.slice!(0,1)
+       else 
+       	puts "no"
+        total += numerals[roman[0..1]]
+      	roman.slice!(0, 2)
+       end
+   end   
+  total 
 end
 
-puts solution("MCMVIII")
+puts solution("MDCLXVI")
