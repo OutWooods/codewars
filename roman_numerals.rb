ba13bd527d58d7f561930607c81ae4b3f2ff8426
Tom Grand <thomas.a.grand@gmail.com>
2017-12-22 17:40:56 +0000

    Add dictionary Hash for numerals

---
+++ roman_numerals.rb
@@ -0,0 +1,8 @@
+def solution(roman)
+ numerals = {"I" => 1, "IV" => 4,"V" => 5, "IX" => 9, "X" => 10,
+             "XL" => 40, "L" => 50,"XC" => 90, "C" => 100, "D" => 100, 
+              "CM" => 900, "M" => 1000}
+ 
+end
+
+puts solution("MCMVIII")
