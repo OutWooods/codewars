2c8b0855f1e82967a97fadce0f79b45c922d4875
Tom Grand <thomas.a.grand@gmail.com>
2017-12-22 18:15:48 +0000

    Revert back to recursive solution
    Felt this was was more intuitive and clear

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,31 +1,24 @@
 
-def two_numerals_to_number(digits)
- numerals = {"IV" => 4, "IX" => 9, "XL" => 40, "XC" => 90, "CM" => 900, }
-  return   false    if  numerals[digits].nil?
-  numerals[digits]
-end
 
-def single_numeral_to_number(digit)
-   numerals = {"I" => 1, "IV" => 4,"V" => 5, "X" => 10, "L" => 50, "C" => 100, 
-   	          "D" => 500, "CM" => 900, "M" => 1000} 
-   return   false    if  numerals[digit].nil?
-   numerals[digit]           
-end
 
-def numerals_to_number(roman, total=0) 
- 
+
+
+def solution(roman, total=0) 
+numerals = {"I" => 1, "IV" => 4,"V" => 5, "IX" => 9, "X" => 10,
+             "XL" => 40, "L" => 50,"XC" => 90, "C" => 100, "D" => 500, 
+              "CM" => 900, "M" => 1000}
+
  return total if roman.length == 0
- return total += single_numeral_to_number(roman) if roman.length == 1
- 
- next_digits = two_numerals_to_number(roman[0..1]) 
- if next_digits == false 
- 	 total += single_numeral_to_number(roman[0]) 
- 	roman.slice!(0, 1)
- else 
-    total += next_digits
-     roman.slice!(0,2)
- end
- total = numerals_to_number(roman, total)
+ return total += numerals[roman] if roman.length == 1
+
+   if numerals[roman[0..1]].nil? 
+        total += numerals[roman[0]]
+       	roman.slice!(0,1)
+   else 
+        total += numerals[roman[0..1]]
+      	roman.slice!(0, 2)
+   end   
+  total = solution(roman, total)
 end
 
-puts numerals_to_number("MDCLXVI")
+puts solution("MDCLXVI")
